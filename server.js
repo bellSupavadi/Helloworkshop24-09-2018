58dbbbbcff37bdfc84b10b2f9cbc601db69724b9
bellSupavadi <supavadi_bell@kjst.ac.th>
2018-09-24 17:35:44 +0700

    update

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var pgp = require('pg-promise')();
 var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
 var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 //app.use(express.static ('static') );
 app.set('view engine', 'ejs');
 
@@ -91,7 +95,10 @@ app.get('/', function(req, res) {
      
                  });
 
+//update data
+app.post('/products/update', function(req, res) {
 
+});
 
   console.log('Appp is running at http://localhost:8080');          
 
