2f39ed39d0bf68caa728152be63f4f3fe42c6805
TOE <toe.rectoe@gmail.com>
2018-10-01 16:15:05 +0700

    heroku

--- server.js
+++ server.js
@@ -5,111 +5,114 @@ var app = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //app.use(express.static ('static') );
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('pages/index');
-        
-    });
 
-    app.get('/about', function(req, res) {
-        var name = ['BUBBLE'];
-        var hobbies = ['Music','Movie','Programming'];
-        var bdate ='27/03/1997';
-        res.render('pages/about',{fullname : name, hobbies:hobbies,bdate: bdate});
-            
-        });
-    //Display all products
-      app.get('/products', function(req, res) {
-            var id = req.param('id');
-            var sql='select* from products';
-                if(id){
-                    sql += ' where id ='+id;
-                }
-           db.any(sql)
-            .then(function(data){
-                console.log('DATA:'+data);
-                res.render('pages/products',{products: data})
-                
-            })
-            .catch(function(error){
-                console.log('ERROR:'+error);
-            })
+});
 
-        });
+app.get('/about', function (req, res) {
+    var name = ['BUBBLE'];
+    var hobbies = ['Music', 'Movie', 'Programming'];
+    var bdate = '27/03/1997';
+    res.render('pages/about', { fullname: name, hobbies: hobbies, bdate: bdate });
 
-    //GET products pid
-        app.get('/products/:pid', function(req, res) {
-            var pid = req.params.pid;
-            var sql = "select * from products where id= " + pid;
-         db.any(sql)
-            .then(function(data){
-                console.log('DATA:'+data);
-                res.render('pages/products_edit',{product: data[0]})
-                
-            })
-            .catch(function(error){
-                console.log('ERROR:'+error);
-            })
-  
-           
-        });
+});
+//Display all products
+app.get('/products', function (req, res) {
+    var id = req.param('id');
+    var sql = 'select* from products';
+    if (id) {
+        sql += ' where id =' + id;
+    }
+    db.any(sql)
+        .then(function (data) {
+            console.log('DATA:' + data);
+            res.render('pages/products', { products: data })
 
-        //Display all user
-        app.get('/users', function(req, res) {
-            var id = req.param('id');
-            var sql='select* from users';
-            if(id){
-                sql += ' where id ='+id;
-            }
-            db.any(sql)
-             .then(function(data){
-                 console.log('DATA:'+data);
-                 res.render('pages/users',{users: data})
-                 
-             })
-             .catch(function(error){
-                 console.log('ERROR:'+error);
-             })
- 
-             });
-   //Display all user id
-            app.get('/users/:id', function(req, res) {
-                var id = req.param('id');
-                var sql='select* from users';
-                if(id){
-                    sql += ' where id ='+id;
-                }
-                db.any(sql)
-                 .then(function(data){
-                     console.log('DATA:'+data);
-                     res.render('pages/users',{users: data})
-                     
-                 })
-                 .catch(function(error){
-                     console.log('ERROR:'+error);
-                 })
-     
-                 });
+        })
+        .catch(function (error) {
+            console.log('ERROR:' + error);
+        })
+
+});
+
+//GET products pid
+app.get('/products/:pid', function (req, res) {
+    var pid = req.params.pid;
+    var sql = "select * from products where id= " + pid;
+    db.any(sql)
+        .then(function (data) {
+            console.log('DATA:' + data);
+            res.render('pages/products_edit', { product: data[0] })
+
+        })
+        .catch(function (error) {
+            console.log('ERROR:' + error);
+        })
+
+
+});
+
+//Display all user
+app.get('/users', function (req, res) {
+    var id = req.param('id');
+    var sql = 'select* from users';
+    if (id) {
+        sql += ' where id =' + id;
+    }
+    db.any(sql)
+        .then(function (data) {
+            console.log('DATA:' + data);
+            res.render('pages/users', { users: data })
+
+        })
+        .catch(function (error) {
+            console.log('ERROR:' + error);
+        })
+
+});
+//Display all user id
+app.get('/users/:id', function (req, res) {
+    var id = req.param('id');
+    var sql = 'select* from users';
+    if (id) {
+        sql += ' where id =' + id;
+    }
+    db.any(sql)
+        .then(function (data) {
+            console.log('DATA:' + data);
+            res.render('pages/users', { users: data })
+
+        })
+        .catch(function (error) {
+            console.log('ERROR:' + error);
+        })
+
+});
 
 //update data
-app.post('/products/update', function(req, res) {
+app.post('/products/update', function (req, res) {
 
     var id = req.body.id;
     var title = req.body.title;
     var price = req.body.price;
-   // var sql = 'update products set title : "'+title+ '" price : "'+price+ '" where id : '+id;  วิธีต่อเเบบไม่ดี
-  // Alt+96 = ``
-   var sql =  `update products set title : ${title} price : ${price} where id : ${id}`
-   //db.none
-   console.log('UPDATE : '+sql);
+    // var sql = 'update products set title : "'+title+ '" price : "'+price+ '" where id : '+id;  วิธีต่อเเบบไม่ดี
+    // Alt+96 = ``
+    var sql = `update products set title : ${title} price : ${price} where id : ${id}`
+    //db.none
+    console.log('UPDATE : ' + sql);
     res.redirect('/products');
 
 });
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+    console.log('App is running on http://localhost:' + port);
+});
+//   console.log('Appp is running at http://localhost:8080');          
 
-  console.log('Appp is running at http://localhost:8080');          
-
-app.listen(8080);
+// app.listen(8080);
