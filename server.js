8e28ed230a22624e11c8c1cfead842b15c717ff4
Supavadi <supavadi_bell@kjst.ac.th>
2018-10-19 21:16:52 +0700

    edit server

--- server.js
+++ server.js
@@ -44,37 +44,24 @@ app.get('/products/:pid', function (req, res) {
 
 
 //Display all products
-// app.get('/products', function (req, res) {
-//     var id = req.param('id');
-//     var sql = 'select* from products';
-//     if (id) {
-//         sql += ' where id =' + id;
-//     }
-//     db.any(sql)
-//         .then(function (data) {
-//             console.log('DATA:' + data);
-//             res.render('pages/products', { products: data })
-
-//         })
-//         .catch(function (error) {
-//             console.log('ERROR:' + error);
-//         })
-
-// });
-
 app.get('/products', function (req, res) {
-    var id = req.param('id');
-    db.any('select* from products', )
+    var id = request.param('id');
+    var sql = 'select* from products';
+    if (id) {
+        sql += ' where id =' + id;
+    }
+    db.any(sql)
         .then(function (data) {
-            console.log('DATA' + data);
-            res.render('pages/products', { products: data })
+            console.log('DATA:' + data);
+            res.render('pages/products', { products: data });
 
         })
         .catch(function (error) {
-            console.log('ERROR:' + error);
+            console.log('ERROR:' + console.error);
         })
 
 });
+
 
 
 
