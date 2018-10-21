d63d12182bf1a0c784e5ab97cddb92351dcfe442
Supavadi <supavadi_bell@kjst.ac.th>
2018-10-21 16:34:59 +0700

    sss

--- server.js
+++ server.js
@@ -192,7 +192,7 @@ var sql=`update products set title='${title}',price=${price} where id=${id}`;
 db.query(sql);
     res.redirect('/products')    
 db.close();
-})
+}) 
 
 //update users
 app.post('/users/update',function (req,res) {
