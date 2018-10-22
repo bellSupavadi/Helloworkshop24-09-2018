1637893b0b5a59db18923437dc68fed87a8cbef4
Supavadi <supavadi_bell@kjst.ac.th>
2018-10-22 17:01:35 +0700

    server

--- server.js
+++ server.js
@@ -237,19 +237,20 @@ app.post('/users/update',function (req,res) {
     });
 
     //report user
-    app.get('/report_user', function(req, res) {
-        var sql='select purchases.user_id,purchases.name,users.email,sum(purchase_items.price) as price from purchases inner join users on users.user_id=purchases.user_id inner join purchase_items on purchase_items.purchase_id=purchases.purchase_id group by purchases.user_id,purchases.name,users.email order by sum(purchase_items.price) desc LIMIT 5;'
-        db.any(sql)
-            .then(function (data) 
-            {
-                //console.log('DATA' + data);
-                res.render('pages/report_user', { user : data });
-            })
-            .catch(function (data) 
-            {
-                console.log('ERROR' + error);
-            })
-    });
+    
+app.get('/report_user', function(req, res) {
+    var sql='select purchases.user_id,purchases.name,users.email,sum(purchase_items.price) as price from purchases inner join users on users.user_id=purchases.user_id inner join purchase_items on purchase_items.purchase_id=purchases.purchase_id group by purchases.user_id,purchases.name,users.email order by sum(purchase_items.price) desc LIMIT 25;'
+    db.any(sql)
+        .then(function (data) 
+        {
+            // console.log('DATA' + data);
+            res.render('pages/report_user', { user : data });
+        })
+        .catch(function (data) 
+        {
+            console.log('ERROR' + error);
+        })
+});
     
 
 
